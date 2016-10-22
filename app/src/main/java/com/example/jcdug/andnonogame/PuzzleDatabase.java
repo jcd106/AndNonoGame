3f68e9f48a963d0b9cc9cd932d1902cf73b50284
jcd106 <jcd106@cwru.edu>
2016-10-22 18:05:35 -0400

    Added a puzzle to PuzzleDatabase

--- app/src/main/java/com/example/jcdug/andnonogame/PuzzleDatabase.java
+++ app/src/main/java/com/example/jcdug/andnonogame/PuzzleDatabase.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
@@ -35,15 +38,26 @@ public class PuzzleDatabase extends SQLiteOpenHelper {
         String[] r = {"1","3","5","2 2","5"};
         String[] c = {"3","4","3 1","4","3"};
         Puzzle firstPuzzle = new Puzzle(id,s,sol,r,c);
+        insertPuzzle(id, firstPuzzle, s);
     }
 
     public void insertPuzzle(int id, Puzzle p, int[] s){
-        SQLiteDatabase db = getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(colID, id);
-        contentValues.put(row, s[0]);
-        contentValues.put(col, s[1]);
-        db.insert(puzzleTable, null, contentValues);
+        try{
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(bos);
+            out.writeObject(p);
+            out.close();
+            byte[] buf = bos.toByteArray();
+            SQLiteDatabase db = getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(colID, id);
+            contentValues.put(puzzle, buf);
+            contentValues.put(row, s[0]);
+            contentValues.put(col, s[1]);
+            db.insert(puzzleTable, null, contentValues);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
