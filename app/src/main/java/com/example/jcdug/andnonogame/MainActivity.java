c782fe7346735d3672e6f5d9bf10f7b5cbc3bf77
jcd106 <jcd106@cwru.edu>
2016-10-22 11:48:54 -0400

    Added blank activity for TutorialActivity.  Created MainActivity layout and added button transitions for MainActivity

--- app/src/main/java/com/example/jcdug/andnonogame/MainActivity.java
+++ app/src/main/java/com/example/jcdug/andnonogame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jcdug.andnonogame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPlayClicked(View view)
+    {
+        Intent i = new Intent(this, SizeSelectActivity.class);
+        startActivity(i);
+    }
+
+    public void onTutorialClicked(View view)
+    {
+        Intent i = new Intent(this, TutorialActivity.class);
+        startActivity(i);
+    }
+
+    public void onSettingsClicked(View view)
+    {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
     }
 }
