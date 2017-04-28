98183f8ec3dba37713e05ccfe166bc08277e8e29
Peter <petertodorov95@yahoo.com>
2017-04-27 22:45:51 -0400

    Added location for downloaded puzzles in menu flow

--- app/src/main/java/com/example/jcdug/andnonogame/PuzzleSelectActivity.java
+++ app/src/main/java/com/example/jcdug/andnonogame/PuzzleSelectActivity.java
@@ -60,16 +60,11 @@ public class PuzzleSelectActivity extends AppCompatActivity implements BarFragme
         Intent i = getIntent();
         String size = i.getStringExtra("size");
         isColor = i.getBooleanExtra("color", false);
+        table = i.getStringExtra("table");
         if (size.equals("your")) {
 
             TextView sizeText = (TextView) findViewById(R.id.puzzle_select_size);
             sizeText.setText("Your Puzzles");
-
-            if (isColor) {
-                table = getString(R.string.yourColorTable);
-            } else {
-                table = getString(R.string.yourTable);
-            }
 
             //Get the puzzle database
             PuzzleDatabase db = MainActivity.getDB();
@@ -152,7 +147,94 @@ public class PuzzleSelectActivity extends AppCompatActivity implements BarFragme
                 //Set prevId to the current button's id
                 prevId = id;
             }
-        } else {
+        }
+        else if(size.equals("down")){
+            TextView sizeText = (TextView) findViewById(R.id.puzzle_select_size);
+            sizeText.setText("Downloaded Puzzles");
+
+            //Get the puzzle database
+            PuzzleDatabase db = MainActivity.getDB();
+
+            //Query the database for all puzzles with the selected size
+            Cursor c1 = db.getAllDownPuzzles(table);
+
+            //Move the cursor to the first tuple
+            c1.moveToFirst();
+
+            //Get the index of puzzle id attribute in the query
+            int i1 = c1.getColumnIndex("PuzzleID");
+            int complete = c1.getColumnIndex("Complete");
+
+            //Set prevId to the id of the text view
+            int prevId = R.id.puzzle_select_size;
+            int belowId = R.id.puzzle_select_size;
+            int count = 0;
+
+            //Create a new OnClickListener for a puzzle being selected
+            View.OnClickListener listener = new View.OnClickListener() {
+                //When the button is clicked, PuzzleActivity is started with the id of the chosen puzzle
+                @Override
+                public void onClick(View view) {
+                    Intent i;
+                    if (isColor) {
+                        i = new Intent(context, ColorPuzzleActivity.class);
+                    } else {
+                        i = new Intent(context, PuzzleActivity.class);
+                    }
+                    i.putExtra("table", table);
+                    i.putExtra("puzzleID", Integer.toString(view.getId()));
+                    startActivity(i);
+                }
+            };
+            //While there are still tuples for the cursor to read
+            while (!c1.isAfterLast()) {
+                final float scale = this.getResources().getDisplayMetrics().density;
+                int pixels = (int) (125 * scale + 0.5f);
+
+                //Create a layout and set the margins for the puzzle select button
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(pixels, pixels);
+                //        (RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+                params.setMargins(40, 20, 40, 20);
+
+                //Create a new button
+                //Button b = new Button(this);
+                LayoutInflater li = LayoutInflater.from(this);
+                Button b = (Button) li.inflate(R.layout.puzzle_button, puzzleSelectLayout, false);
+
+                //Get the id from the cursor and set the button's id and text
+                int id = c1.getInt(i1);
+                int isComp = c1.getInt(complete);
+                b.setId(id);
+                if (isComp == 1)
+                    b.setText("Puzzle: " + id + " is complete");
+                else
+                    b.setText("Puzzle: " + id + " is incomplete");
+
+                //Set the button's background and OnClickListener
+                //b.setBackgroundResource(R.drawable.border_button);
+                b.setOnClickListener(listener);
+
+                //Make sure the next button is below prevId
+                if (count % 2 == 0) {
+                    params.addRule(RelativeLayout.BELOW, prevId);
+                    belowId = prevId;
+                } else {
+                    params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                    params.addRule(RelativeLayout.BELOW, belowId);
+                }
+
+                count++;
+
+                puzzleSelectLayout.addView(b, params);
+
+                //Move the cursor to the next tuple
+                c1.moveToNext();
+
+                //Set prevId to the current button's id
+                prevId = id;
+            }
+        }
+        else {
             String[] rxc = size.split(" ");
             int[] s = {Integer.parseInt(rxc[0]), Integer.parseInt(rxc[1])};
 
@@ -164,11 +246,6 @@ public class PuzzleSelectActivity extends AppCompatActivity implements BarFragme
             PuzzleDatabase db = MainActivity.getDB();
 
             //Query the database for all puzzles with the selected size
-            if (isColor) {
-                table = getString(R.string.colorTable);
-            } else {
-                table = getString(R.string.puzzleTable);
-            }
             Cursor c1 = db.getPuzzlesBySize(table, s[0], s[1]);
 
             //Move the cursor to the first tuple
