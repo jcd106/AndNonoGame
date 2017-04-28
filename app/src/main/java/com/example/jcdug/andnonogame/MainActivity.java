42c1e8fafcafe5cd960a8bded3cce7d0b0884591
Josh Dughi <jcd106@case.edu>
2017-04-28 11:05:09 -0400

    Possibly fixed bug with login token expiring

--- app/src/main/java/com/example/jcdug/andnonogame/MainActivity.java
+++ app/src/main/java/com/example/jcdug/andnonogame/MainActivity.java
@@ -120,15 +120,14 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onStart() {
         super.onStart();
-
         OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-        if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
-            Log.d(TAG, "Got cached sign-in");
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
-        } else {
+//        if (opr.isDone()) {
+//            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+//            // and the GoogleSignInResult will be available instantly.
+//            Log.d(TAG, "Got cached sign-in");
+//            GoogleSignInResult result = opr.get();
+//            handleSignInResult(result);
+//        } else {
             // If the user has not previously signed in on this device or the sign-in has expired,
             // this asynchronous branch will attempt to sign in the user silently.
             // Cross-device single sign-on will occur in this branch.
@@ -138,7 +137,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                     handleSignInResult(googleSignInResult);
                 }
             });
-        }
+        //}
     }
 
     @Override
